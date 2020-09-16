189191683aa7d1b560ed65fbf2e2a0a4cb114f14
Gabriel Gomes <gabrielsantosgomes9@gmail.com>
2020-09-15 21:18:13 -0300

    Atualizando repositório

---
+++ interface1/src/model/services/RentalService.java
@@ -0,0 +1,37 @@
+package model.services;
+
+import model.entities.CarRental;
+import model.entities.Invoice;
+
+public class RentalService {
+
+	private Double pricePerHour;
+	private Double pricePerDay;
+	
+	private TaxService taxService;
+
+	public RentalService(Double pricePerHour, Double pricePerDay, TaxService taxService) {
+		super();
+		this.pricePerHour = pricePerHour;
+		this.pricePerDay = pricePerDay;
+		this.taxService = taxService;
+	}
+
+	public void processInvoice(CarRental carRental) {
+		long t1 = carRental.getStart().getTime();
+		long t2 = carRental.getFinish().getTime();
+		double hours = t2 - t1;
+		
+		double basicPayment;
+		if(hours <= 12.0) {
+			basicPayment = pricePerHour * Math.ceil(hours);
+		}
+		else {
+			basicPayment = pricePerDay * Math.ceil(hours);
+		}
+		
+		double tax = taxService.tax(basicPayment);
+		
+		carRental.setInvoice(new Invoice(basicPayment, tax));
+	}
+}
